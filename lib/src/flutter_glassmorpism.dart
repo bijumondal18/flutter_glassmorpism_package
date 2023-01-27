07f930fbe97086cd3dcf8d66ae17964dff72f9e2
Biju <biju.mondal@app.cbnits.com>
2023-01-27 14:37:17 +0530

    final

--- lib/src/flutter_glassmorpism.dart
+++ lib/src/flutter_glassmorpism.dart
@@ -1,63 +1,44 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class GlassmorphicContainer extends StatefulWidget {
+class GlassmorphicContainer extends StatelessWidget {
   final double? width;
   final double? height;
+  final double? padding;
+  final double? margin;
+  final double? radius;
+  final double? border;
+  final Color? backgroundColor;
   final Widget child;
-  final double? borderRadius;
-  final Color? borderColor;
-  final EdgeInsets? padding;
-  final EdgeInsets? margin;
-  final VoidCallback? onPressed;
 
   const GlassmorphicContainer(
       {Key? key,
-      this.width,
-      this.height,
+      this.width = 200,
+      this.height = 200,
+      this.padding = 10,
+      this.margin = 10,
       required this.child,
-      this.borderRadius,
-      this.borderColor,
-      this.onPressed,
-      this.padding,
-      this.margin})
+      this.radius = 10,
+      this.border = 1,
+      this.backgroundColor = Colors.white})
       : super(key: key);
 
   @override
-  State<GlassmorphicContainer> createState() => _GlassmorphicContainerState();
-}
-
-class _GlassmorphicContainerState extends State<GlassmorphicContainer> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onPressed,
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
       child: Container(
-        width: widget.width ?? MediaQuery.of(context).size.width,
-        height: widget.height ?? MediaQuery.of(context).size.height,
-        padding: widget.padding ?? EdgeInsets.zero,
-        margin: widget.margin ?? EdgeInsets.zero,
+        padding: EdgeInsets.all(padding!),
+        margin: EdgeInsets.all(margin!),
+        width: width!,
+        height: height!,
         decoration: BoxDecoration(
-            border:
-                Border.all(width: 1, color: widget.borderColor ?? Colors.white),
-            borderRadius: BorderRadius.circular(widget.borderRadius ?? 10.0),
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.white.withOpacity(0.2),
-                  Colors.white.withOpacity(0.5),
-                ])),
-        child: widget.child,
+            border: Border.all(
+                width: border!, color: backgroundColor!.withOpacity(0.3)),
+            color: backgroundColor!.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(radius!)),
+        child: child,
       ),
     );
   }
